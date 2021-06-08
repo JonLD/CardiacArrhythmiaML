50b977807e2417bd422022168e1c48f8f23491db
Josh Heaton <jj_heaton@yahoo.co.uk>
2021-06-08 16:19:22 +0100

    f1 for classes

--- ECG/Model/ecg_model.py
+++ ECG/Model/ecg_model.py
@@ -3,6 +3,7 @@ from pytorch_lightning.core.lightning import LightningModule
 from torch import nn
 from torch.nn import functional as F
 from torchmetrics.functional import accuracy, f1
+from sklearn.metrics import classification_report
 
 
 class MLECG(LightningModule):
@@ -21,11 +22,13 @@ class MLECG(LightningModule):
         self.layer2 = nn.Conv1d(16, 32, self.kernel_size)
         self.bnorm2 = nn.BatchNorm1d(32)
         self.maxp2 =  nn.MaxPool1d(7)
-        self.layer3 = nn.Conv1d(32, 64, self.kernel_size)
-        self.bnorm3 = nn.BatchNorm1d(64)
-        self.maxp3 =  nn.MaxPool1d(3)
-        self.rnn = nn.LSTM(64, self.target_class, self.cell_count, dropout=self.dropout, batch_first=True)
-        self.dense = nn.Linear(39, 1)
+#        self.layer3 = nn.Conv1d(32, 64, self.kernel_size)
+#        self.bnorm3 = nn.BatchNorm1d(64)
+#        self.maxp3 =  nn.MaxPool1d(3)
+        #3x conv 64 2x 32 1x 16
+        self.rnn = nn.LSTM(32, self.target_class, self.cell_count, dropout=self.dropout, batch_first=True)
+        #3x conv 39 2x 127 1x 899
+        self.dense = nn.Linear(127, 1)
     
     def forward(self, x):
         x = self.layer1(x)
@@ -36,15 +39,13 @@ class MLECG(LightningModule):
         x = F.relu(x)
         x = self.bnorm2(x)
         x = self.maxp2(x)
-        x = self.layer3(x)
-        x = F.relu(x)
-        x = self.bnorm3(x)
-        x = self.maxp3(x)
+#        x = self.layer3(x)
+#        x = F.relu(x)
+#        x = self.bnorm3(x)
+#        x = self.maxp3(x)
         x = torch.transpose(x, 1, 2)
-#        x = x.reshape(self.batch_size, 449, 32)
         x, _ = self.rnn(x)
         x = torch.transpose(x, 1, 2)
-#        x = x.reshape(self.batch_size, 4, 449)
         x = self.dense(x)
         x = x.reshape(self.batch_size, 4)
         return x
@@ -90,7 +91,9 @@ class MLECG(LightningModule):
         self.log('test/f1', f_score, prog_bar=True)
         self.log('test/loss', loss, prog_bar=True)
         self.log('test/accuracy', acc, prog_bar=True)
-        return {"test_loss": loss, "test_accuracy": acc, "f_score" : f_score}
+        predictions = torch.argmax(probs, dim=1)
+        targets = torch.argmax(y, dim=1)
+        return {"test_loss": loss, "test_accuracy": acc, "f_score" : f_score, "predictions" : predictions, "targets" : targets}
     
     def training_epoch_end(self, training_step_outputs):
         avg_loss = torch.stack(
@@ -121,6 +124,12 @@ class MLECG(LightningModule):
             [x["test_accuracy"] for x in test_step_outputs]).mean()
         avg_f = torch.stack(
             [x["f_score"] for x in test_step_outputs]).mean()
+        target_names = ['A', 'N', 'O', '~']
+        combined_pred = torch.cat(
+            [x["predictions"] for x in test_step_outputs], 0)
+        combined_target = torch.cat(
+            [x["targets"] for x in test_step_outputs], 0)
+        print(classification_report(combined_target.cpu(), combined_pred.cpu(), target_names=target_names))
         self.log("ptl/test_f_score", avg_f)
         self.log("ptl/test_loss", avg_loss)
         self.log("ptl/test_accuracy", avg_acc)
